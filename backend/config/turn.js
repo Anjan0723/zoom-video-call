e2cd146792f60cb4efa4c6374acb66901f1cffc9
Anjan0723 <anjankumar3552@gmail.com>
2026-01-04 15:08:45 +0530

    try alternative turn servers

--- backend/config/turn.js
+++ backend/config/turn.js
@@ -2,16 +2,20 @@ module.exports = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19302" },
     {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject"
+      urls: "turn:numb.viagenie.ca",
+      username: "webrtc@live.com",
+      credential: "muazkh"
     },
     {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject",
-      credential: "openrelayproject"
+      urls: "turn:relay.metered.ca:80",
+      username: "d0a13e0472c6803d3ddc6dd6",
+      credential: "d0a13e0472c6803d3ddc6dd6"
+    },
+    {
+      urls: "turn:relay.metered.ca:443",
+      username: "d0a13e0472c6803d3ddc6dd6",
+      credential: "d0a13e0472c6803d3ddc6dd6"
     }
   ]
 };
